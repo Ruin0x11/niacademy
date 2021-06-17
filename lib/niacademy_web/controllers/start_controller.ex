f0ecd35bcd0ae090b9f732284bffbabb6fd12473
Ruin0x11 <ipickering2@gmail.com>
2021-06-16 17:58:48 -0700

    Ratio of time spent in start screen

--- lib/niacademy_web/controllers/start_controller.ex
+++ lib/niacademy_web/controllers/start_controller.ex
@@ -2,6 +2,10 @@ defmodule NiacademyWeb.StartController do
   use NiacademyWeb, :controller
   alias Niacademy.Tracking
 
+  defp clamp(n, min, _max) when n<min, do: min
+  defp clamp(n, _min, max) when n>max, do: max
+  defp clamp(n, _min, _max), do: n
+
   def index(conn, _params) do
     Tracking.stop_tracking_active
 
@@ -9,13 +13,16 @@ defmodule NiacademyWeb.StartController do
     preset = Niacademy.Db.list_presets[preset_id]
     project_type = Niacademy.Db.get_optimal_project()
 
-    %{tutorial: tutorial, free: free} = Niacademy.Tracking.Cache.get_stats()
+    %{tutorial: tutorial, free: free, ratio: ratio} = Niacademy.Tracking.Cache.get_stats()
+
+    percentage = clamp((tutorial / (tutorial + free)) * 100, 0, 100)
 
     render(conn, "index.html",
       preset_id: preset_id,
       preset: preset,
       tutorial_draw_time: tutorial,
       free_draw_time: free,
+      draw_time_percentage: percentage,
       project_type: project_type)
   end
 end
